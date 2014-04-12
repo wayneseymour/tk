a3d8a200be955119f8ab471d4b5ebc86e501ebdf
Wayne Kenneth Seymour III <wayneseymour@gmail.com>
2014-04-12 08:57:44 -0400

    File sys cleanup.

---
+++ test/unit/controllers/example.js
@@ -0,0 +1,23 @@
+/*global describe, beforeEach, expect, inject*/
+'use strict';
+
+describe('Example Test', function() {
+  var element;
+  var $scope;
+  var $rootScope;
+  var $compile;
+
+  beforeEach(inject(function(_$compile_, _$rootScope_) {
+    $compile = _$compile_;
+    $rootScope = _$rootScope_;
+    $scope = $rootScope;
+
+    element = angular.element('<div>{{2 + 2}}</div>');
+    element = $compile(element)($rootScope);
+  }));
+
+  it('should equal 4', function() {
+    $scope.$digest();
+    expect(element.html()).toBe('4');
+  });
+});
